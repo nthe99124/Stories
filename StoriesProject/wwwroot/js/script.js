fad892f65229eebf150175182de433829bbcc138
vinhtrt17 <vinhtthe153663@fpt.edu.vn>
2024-03-09 06:58:50 +0700

    Upload admin page

---
+++ StoriesProject/wwwroot/js/script.js
@@ -0,0 +1,31 @@
+﻿
+function toggleSidebar() {
+    const hamBurger = document.querySelector(".toggle-btn");
+
+    hamBurger.addEventListener("click", function () {
+        document.querySelector("#sidebar").classList.toggle("expand");
+    });
+}
+
+window.registerMouseEvents = () => {
+    const showComicInfo = document.getElementById('showComicInfo');
+
+    document.querySelectorAll('tr').forEach(row => {
+        row.addEventListener('mouseenter', (event) => {
+            const mouseX = event.clientX;
+            const mouseY = event.clientY;
+
+            showComicInfo.style.left = `${mouseX}px`;
+            showComicInfo.style.top = `${mouseY}px`;
+            showComicInfo.style.display = 'block';
+        });
+
+        row.addEventListener('mouseleave', () => {
+            showComicInfo.style.display = 'none';
+        });
+    });
+
+    showComicInfo.addEventListener('mouseleave', () => {
+        showComicInfo.style.display = 'none';
+    });
+};
