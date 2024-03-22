9368eea9e0a491cf628230264e0146acbfacff11
vinh tran <vinhtthe153663@fpt.edu.vn>
2024-03-22 06:39:51 +0000

    Merged PR 28: fix fe edit chapter, my comic
    fix fe edit chapter, my comic

---
+++ StoriesProject.Model/DTO/Chapter/ChapterForEditDTO.cs
@@ -0,0 +1,18 @@
+﻿using StoriesProject.Model.BaseEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoriesProject.Model.DTO.Chapter
+{
+    public class ChapterForEditDTO
+    {
+        public List<string> ImgLinkContent { get; set; }
+        public string StoryName { get; set; }
+        public string ChapterName { get; set; }
+        public Guid ChapterId  { get; set; }
+
+    }
+}
